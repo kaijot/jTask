c729c7e51982e441486cee4f4b9a4f1821c0a9a9
kaijot <decanabolps3@gmail.com>
2018-03-21 15:49:13 +0100

    Improvements in task class and form

--- src/main/java/pl/codespring/testproject/task/Task.java
+++ src/main/java/pl/codespring/testproject/task/Task.java
@@ -3,15 +3,47 @@ package pl.codespring.testproject.task;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 import java.util.Objects;
+
+enum TaskPriority {LOW, MEDIUM, HIGH}
+
 @Entity
 public class Task {
     @Id
     @GeneratedValue
-     int id;
-     String name;
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime toDoDate;
+    private TaskPriority priority;
 
 
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getToDoDate() {
+        return toDoDate;
+    }
+
+    public void setToDoDate(LocalDateTime toDoDate) {
+        this.toDoDate = toDoDate;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
 
     public int getId() {
 
@@ -29,24 +61,23 @@ public class Task {
     public void setName(String name) {
         this.name = name;
     }
+
     {
+        this.name = "Task name";
+        this.toDoDate = LocalDateTime.now();
+        this.description = "Task description";
 
     }
 
-    public Task(){
+    public Task() {
 
     }
-    public Task(int id, String name) {
 
-        this.id = id;
+    public Task(String name, String description, LocalDateTime toDoDate, TaskPriority priority) {
         this.name = name;
-    }
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        this.description = description;
+        this.toDoDate = toDoDate;
+        this.priority = priority;
     }
 
     @Override
@@ -55,12 +86,26 @@ public class Task {
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
         return id == task.id &&
-                Objects.equals(name, task.name);
+                Objects.equals(name, task.name) &&
+                Objects.equals(description, task.description) &&
+                Objects.equals(toDoDate, task.toDoDate) &&
+                priority == task.priority;
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, name);
+        return Objects.hash(id, name, description, toDoDate, priority);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", toDoDate=" + toDoDate +
+                ", priority=" + priority +
+                '}';
     }
 }
